85fc0c1eecbd1788b6aa9ee90201ab341f40c88f
unrevealedbd77-create <unrevealedbd77@gmail.com>
2026-01-21 12:13:29 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/academic/', // Replace 'your-repo-name' with your actual repository name
+  base: '/academic/', 
 })
